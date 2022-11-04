96f4273d27898312bf586d0938ffd1e1db5c1e71
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-04 20:48:51 +0100

    Mesa de Som

---
+++ JavaScript/Projeto-Midia/main.js
@@ -0,0 +1,45 @@
+function playPom(){
+document.querySelector(".tecla tecla_pom");
+document.querySelector("#som_tecla_pom").play();
+}
+
+function playClap(){
+    document.querySelector(".tecla_clap");
+    document.querySelector("#som_tecla_clap").play();
+}
+
+function playTim() {
+  document.querySelector(".tecla_tim");
+  document.querySelector("#som_tecla_tim").play();
+}
+
+function playPuff() {
+  document.querySelector(".tecla_puff");
+  document.querySelector("#som_tecla_puff").play();
+}
+
+function playSplash() {
+  document.querySelector(".tecla_splash");
+  document.querySelector("#som_tecla_splash").play();
+}
+
+function playToim() {
+  document.querySelector(".tecla_toim");
+  document.querySelector("#som_tecla_toim").play();
+}
+
+function playPsh() {
+  document.querySelector(".tecla_psh");
+  document.querySelector("#som_tecla_psh").play();
+}
+
+function playTic() {
+  document.querySelector(".tecla_tic");
+  document.querySelector("#som_tecla_tic").play();
+}
+
+function playTom() {
+  document.querySelector(".tecla_tom");
+  document.querySelector("#som_tecla_tom").play();
+}
+
