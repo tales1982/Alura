adf6b66ad463d23d87ecc237de06344867d8a970
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-11-05 23:01:27 +0100

    canvas02

--- JavaScript/Animaçao com javaScript/JS/animacao2.js
+++ JavaScript/Animaçao com javaScript/JS/animacao2.js
@@ -1,7 +1,6 @@
 //CANVA Container02
 
-function desenhaQuadradoVerde(x, y,cor ){
-
+function desenhaQuadradoVerde(x, y, cor ){
 let tela02 = document.querySelector("#canvas02");
 let pincel02 = tela02.getContext("2d")
 
@@ -9,12 +8,23 @@ pincel02.fillStyle = cor;// minha cor
 pincel02.fillRect(x, y, 50 ,50);// o tanhaho do quadrado 50 x 50 forma um quadrado
 pincel02.fillStroke = "black" // forma uma borda em volta do meu quadrado 
 pincel02.strokeRect(x, y, 50 ,50)//falando de onde minha borda deve comesar no casso tem que comecar no mesmo que meu quadrado
+}
 
-let x = 0
 
+//Posso ussar o while ou For testarei os 2 exemplos
+//EXCEMPLO WHILE
+/*
+let x = 0;
 while(x < 600){
     desenhaQuadradoVerde(x, 0, "green");
-    x = x +50;
+    desenhaQuadradoVerde(x, 50, "red");
+    x = x + 50;    
 }
+*/
+//EXEMPLO FOR
+
+for(let x =0; x < 600; x = x +50){
+    desenhaQuadradoVerde(x, 0, "lightgray");
+    
 }
 
