05559225eb57e9b8d7ad5175694b98dadc8fb361
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-13 21:45:41 +0100

    concat()

---
+++ JavaScript/Arrays/004-concat/concat.js
@@ -0,0 +1,11 @@
+//O concat() junta 2 arrays em um array novo.
+
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+console.log(salaJS);
+console.log(salaPython)
+
+const salasJuntas = salaJS.concat(salaPython);//Juntados as 2 salas em uma sala so
+console.log(salasJuntas)
+
