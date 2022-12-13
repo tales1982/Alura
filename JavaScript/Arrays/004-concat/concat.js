d15245246aaa9cff8e47c676af8de4af53024216
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-13 23:02:50 +0100

    concat()

--- JavaScript/Arrays/004-concat/concat.js
+++ JavaScript/Arrays/004-concat/concat.js
@@ -1,5 +1,8 @@
 //O concat() junta 2 arrays em um array novo.
 
+/*Junta dois arrays, colocando o array passado como argumento, logo depois do primeiro. Em português essa operação é conhecida como concatenação.
+Não altera o array no qual foi chamado, então precisamos salvar esse resultado em um novo array.*/
+
 const salaJS = ["Evaldo", "Camis", "Mari"];
 const salaPython = ["Ju", "Leo", "Raquel"];
 
