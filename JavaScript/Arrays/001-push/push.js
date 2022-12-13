e1570c298358927b64ab74821d6c0e03e63cb762
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-13 20:18:25 +0100

    Push()

---
+++ JavaScript/Arrays/001-push/push.js
@@ -0,0 +1,11 @@
+//Um professor acidentalmente passou apenas 3 das 4 notas no sistema para um aluno:
+//Para corrigir, precisamos adicionar a nota 7 e fazer o cálculo da média corretamente.
+
+const notas = [10,6,8]
+
+notas.push(7)//Adicionei uma nota
+
+
+ const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+ console.log(media)
