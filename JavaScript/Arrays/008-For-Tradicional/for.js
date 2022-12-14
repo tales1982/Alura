24cd565e726ddb89d45e37d595444f636d791713
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-14 18:27:00 +0100

    For

--- JavaScript/Arrays/008-For-Tradicional/for.js
+++ JavaScript/Arrays/008-For-Tradicional/for.js
@@ -1,9 +1,13 @@
-const numeros = [100, 200, 300, 400, 500, 600];
-
+const notas = [9.5, 9, 9,8.5];
+let somaDasNotas = 0
 // Primeira expressão: é executada apenas uma única vez
 // Segunda expressão: condição de execução
 // Terceira expressão: é executada sempre ao final do bloco
 
-for (let indice = 0; indice < numeros.length; indice++) {
-  console.log(numeros[indice]);
+for (let i = 0; i < notas.length; i++) {
+  somaDasNotas += notas[i]
 }
+
+const media = somaDasNotas / notas.length
+
+console.log(`A media das notas é ${media}`);
