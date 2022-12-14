f7ef7665b864dd04a5363ffc5b2b4b2417a014a6
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-14 19:31:54 +0100

    pop()

--- JavaScript/Arrays/002-pop/pop.js
+++ JavaScript/Arrays/002-pop/pop.js
@@ -2,13 +2,21 @@
 /*Para remover a última nota, usaremos outro método dos arrays do JavaScript — o pop(). Lembre-se de que, sempre que trabalhamos com métodos, temos que abrir e fechar parênteses:*/
 
 const notas = [10, 6, 8, 5.5, 10];
-console.log(notas);//arrays sem alteração
 
 notas.pop()//elimina o ultimo elemento do array
 
+let somaDasNotas = 0
+
+notas.forEach(nota => somaDasNotas += nota);
+
+const media = somaDasNotas / notas.length
+console.log(`A media das notas é ${media.toFixed(2)}`)
+
+/*
  const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media); 
 
 console.log(notas)//array com a alteração
 
+*/
