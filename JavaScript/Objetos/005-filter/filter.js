55ca69dfa5c41eb2e6c81fd6b8ad9b89ac7527f3
Tales Lima <102876696+tales1982@users.noreply.github.com>
2022-12-15 23:34:25 +0100

    Filtrando um Objetos com o filter()

---
+++ JavaScript/Objetos/005-filter/filter.js
@@ -0,0 +1,44 @@
+const filho = {
+    nome:"Alexandre",
+    idade: 10,
+    cidade:"Alemanha",
+}
+
+
+//ADCIONANDO UMA NOVA PROPRIEDADE => animalDeEstimacao
+filho.animalDeEstimacao = [{
+    animal:"Cachorro",
+    raca:"Dog Alemao",
+    cor:"preto",
+    vivo: true
+}]
+
+
+//ADCIONANDO MAIS UM BICHO AO MEU ARRAY DE => animalDeEstimacao
+
+filho.animalDeEstimacao.push({
+    animal:"Coelho",
+    raca: "Siberiano",
+    cor: "Branco",
+    vivo: false
+})
+
+filho.animalDeEstimacao.push({
+    animal:"cavalo",
+    raca:"Manga-Larga",
+    cor: "Belgi",
+    vivo: true
+})
+//console.log(filho)
+
+
+//Chamando uma funcao para remover animal morto
+
+let animalMorto = filho.animalDeEstimacao.filter(function(animal){
+    return animal.vivo !== true
+})
+
+
+//console.log(filho.animalDeEstimacao)
+console.log(animalMorto)
+    
